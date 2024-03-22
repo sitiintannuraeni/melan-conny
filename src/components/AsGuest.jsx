560227d444e35ea0287c991ead5ec0ff376da7ac
sitiintannuraeni <sitiintannuraeni@smkwikrama.sch.id>
2024-03-23 03:09:55 +0700

    feat : update AsGuest

--- src/components/AsGuest.jsx
+++ src/components/AsGuest.jsx
@@ -1,17 +1,5 @@
 import React, { useEffect } from "react";
-import {
-  Button,
-  Dialog,
-  DialogHeader,
-  DialogBody,
-  DialogFooter,
-  Typography,
-  IconButton,
-  Card,
-  CardBody,
-  Checkbox,
-  CardFooter,
-} from "@material-tailwind/react";
+import { Button, Dialog, Radio, Typography } from "@material-tailwind/react";
 import { useDispatch, useSelector } from "react-redux";
 import { closeDialogAsGuest } from "../slice/menuSlice";
 import { XMarkIcon } from "@heroicons/react/24/outline";
@@ -36,23 +24,23 @@ function AsGuest() {
         open={dialogAsGuest}
         handler={() => dispatch(closeDialogAsGuest())}
       >
-        <div className="p-4 flex justify-between w-[100%]">
-          <Typography className="mt-2 font-medium text-black text-lg">
-            As Guest
-          </Typography>
-          <a
-            href="#"
-            className="flex items-center hover:text-blue-500 text-black transition-colors"
-            onClick={(e) => {
-              e.preventDefault();
-              dispatch(closeDialogAsGuest());
-            }}
-          >
-            <XMarkIcon className="w-6 h-6" />
-          </a>
-        </div>
-        <Typography as="div" className="p-4">
-          <div className="text-md text-[black]">
+        <div className="p-7">
+          <div className="flex justify-between w-[100%]">
+            <Typography className="mt-2 font-medium text-black text-lg">
+              As Guest
+            </Typography>
+            <a
+              href="#"
+              className="flex items-center hover:text-blue-500 text-black transition-colors"
+              onClick={(e) => {
+                e.preventDefault();
+                dispatch(closeDialogAsGuest());
+              }}
+            >
+              <XMarkIcon className="w-6 h-6" />
+            </a>
+          </div>
+          <div className="text-md text-[black] mt-4">
             Phone*
             <div className="flex justify-start items-start space-x-3 mt-3">
               <Typography className="p-3">+62</Typography>
@@ -63,12 +51,39 @@ function AsGuest() {
               />
             </div>
           </div>
-        </Typography>
-        <div className="flex justify-end w-full">
-          <div className="w-[100%] border-b-[1px] border-[#B0B0B0] " />
-        </div>
-        <div className="mt-3 p-4">
-          <Button className="w-full bg-[#B0B0B0]">SEND</Button>
+          <div className="flex justify-end w-full -mt-3">
+            <div className="w-[100%] border-b-[1px] border-[#B0B0B0] " />
+          </div>
+          <div className="text-black mt-7 ">
+            <Typography className="text-sm">Delivery Method*</Typography>
+            <div className="flex flex-col mt-2">
+              <Radio
+                name="terms"
+                label={
+                  <Typography
+                    color="blue-gray"
+                    className="flex font-medium text-sm"
+                  >
+                    SMS
+                  </Typography>
+                }
+              />
+              <Radio
+                name="terms"
+                label={
+                  <Typography
+                    color="blue-gray"
+                    className="flex font-medium text-sm"
+                  >
+                    WHATSAPP
+                  </Typography>
+                }
+              />
+            </div>
+          </div>
+          <div className="mt-3 p-4">
+            <Button className="w-full bg-[#B0B0B0]">SEND</Button>
+          </div>
         </div>
       </Dialog>
     </>

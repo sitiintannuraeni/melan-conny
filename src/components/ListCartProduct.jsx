31d27456d2e4a453d68555a4b7f56fb749c79f32
sitiintannuraeni <sitiintannuraeni@smkwikrama.sch.id>
2024-04-04 11:45:57 +0700

    feat : update DrawerShoppingCart

--- src/components/ListCartProduct.jsx
+++ src/components/ListCartProduct.jsx
@@ -1,114 +1,234 @@
-import { Checkbox, IconButton, Typography } from "@material-tailwind/react";
+import {
+  Button,
+  Checkbox,
+  IconButton,
+  Spinner,
+  Typography,
+} from "@material-tailwind/react";
 import NumberFormatCurrency from "../utils";
 import { MinusIcon, PlusIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import { useDispatch, useSelector } from "react-redux";
 import {
-  decrementQty,
-  incrementQty,
-  removeFromCart,
-  setPriceTotal,
-  setQtyTotal,
-} from "../slice/cartSlice";
+  useDeleteFromBagItemsMutation,
+  useGetBagItemsQuery,
+  useUpdateToBagItemsMutation,
+} from "../services/apiBagsItems";
+import { useMemo } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import EmptyState from "./DrawerShopEmpty";
 
-function ListCardProduct({ image, productName, price }) {
-  const { products } = useSelector((state) => state.cart);
-  const dispatch = useDispatch();
+function UpdateIncrementQty({ id, bagId, quantity, productId, sizeId }) {
+  const [updateItem, { isLoading, isError, isSuccess, error }] =
+    useUpdateToBagItemsMutation();
 
-  // add index value
-  const cartProduct = products.map((prod, idx) => ({ index: idx, ...prod }));
-
-  // sort based on index from high to low
-  cartProduct.sort((a, b) => b.index - a.index);
-
-  const handleIncrementQty = (data) => {
-    dispatch(incrementQty(data));
-    dispatch(setPriceTotal());
-    dispatch(setQtyTotal());
+  const handleIncrementQty = async () => {
+    const body = {
+      bag_id: bagId,
+      product_id: productId,
+      size_id: sizeId,
+      quantity: quantity + 1,
+    };
+    await updateItem({ id: id, data: body });
   };
 
-  const handleDecrementQty = (data) => {
-    dispatch(decrementQty(data));
-    dispatch(setPriceTotal());
-    dispatch(setQtyTotal());
+  if (isLoading) {
+    return (
+      <IconButton
+        variant="outlined"
+        className=" rounded-md h-[25px] w-[25px]"
+        disabled
+      >
+        <Spinner className="h-4 w-4" />
+      </IconButton>
+    );
+  }
+
+  return (
+    <IconButton
+      variant="outlined"
+      className=" rounded-md h-[25px] w-[25px]"
+      onClick={() => handleIncrementQty()}
+    >
+      <PlusIcon className="text-black h-4 w-5" />
+    </IconButton>
+  );
+}
+
+function UpdateDecrementQty({ id, bagId, quantity, productId, sizeId }) {
+  const [updateItem, { isLoading, isError, isSuccess, error }] =
+    useUpdateToBagItemsMutation();
+
+  const handleDecrementQty = async () => {
+    if (quantity > 1) {
+      const body = {
+        bag_id: bagId,
+        product_id: productId,
+        size_id: sizeId,
+        quantity: quantity - 1,
+      };
+      await updateItem({ id: id, data: body });
+      console.log("handleDecrementQty", { body });
+    }
   };
 
-  const handleRemoveFromCart = (data) => {
-    dispatch(removeFromCart(data));
-    dispatch(setPriceTotal());
-    dispatch(setQtyTotal());
-  };
+  if (isLoading) {
+    return (
+      <IconButton
+        variant="outlined"
+        className=" rounded-md h-[25px] w-[25px]"
+        disabled
+      >
+        <Spinner className="h-4 w-4" />
+      </IconButton>
+    );
+  }
+
+  return (
+    <IconButton
+      variant="outlined"
+      className=" rounded-md h-[25px] w-[25px]"
+      onClick={() => handleDecrementQty()}
+    >
+      <MinusIcon className="text-black h-4 w-5" />
+    </IconButton>
+  );
+}
+
+function QuantityItem({ id, bagId, quantity, productId, sizeId }) {
+  const [currentQty, setCurrentQty] = useState(0);
+
+  useEffect(() => {
+    setCurrentQty(quantity);
+  }, [quantity]);
+
   return (
     <>
-      <div className="">
-        {cartProduct.map((product, idx) => {
-          return (
-            <div key={idx} className="flex flex-col-3 mt-4 gap-2">
-              <div className="w-[120px] flex justify-between">
-                <div className="flex justify-start items-start">
-                  <Checkbox className="h-5 w-5" />
-                </div>
-                <div className="flex justify-start items-start">
-                  <img
-                    src={product.image}
-                    alt="drawer1"
-                    className="h-[100px] w-[65px] object-cover rounded-md"
-                  />
-                </div>
-              </div>
-              <div className="gap-1 w-[210px]">
-                <Typography className="text-sm">
-                  {product.product_name}
-                </Typography>
-                <Typography className="text-sm">
-                  <NumberFormatCurrency value={product.qty * product.price} />
-                </Typography>
-                <Typography className="mt-3 text-sm">
-                  {product.size_name}
-                </Typography>
-                <div className="flex justify-start mt-2 space-x-3 w-[140px]">
-                  <IconButton
-                    variant="outlined"
-                    className=" rounded-md h-[25px] w-[25px]"
-                    onClick={() =>
-                      handleDecrementQty({
-                        id: product.id,
-                        size_id: product.size_id,
-                      })
-                    }
-                  >
-                    <MinusIcon className="text-black h-4 w-5" />
-                  </IconButton>
-                  <Typography className="text-lg">{product.qty}</Typography>
-                  <IconButton
-                    variant="outlined"
-                    className=" rounded-md h-[25px] w-[25px]"
-                    onClick={() =>
-                      handleIncrementQty({
-                        id: product.id,
-                        size_id: product.size_id,
-                      })
-                    }
-                  >
-                    <PlusIcon className="text-black h-4 w-5" />
-                  </IconButton>
-                </div>
-              </div>
-              <div
-                className="w-[20px] flex justify-center items-start"
-                onClick={() =>
-                  handleRemoveFromCart({
-                    id: product.id,
-                    size_id: product.size_id,
-                  })
-                }
-              >
-                <XMarkIcon className="h-4 w-4 cursor-pointer" />
-              </div>
-            </div>
-          );
-        })}
+      <div className="flex justify-start mt-2 space-x-3 w-[140px]">
+        <UpdateDecrementQty
+          id={id}
+          bagId={bagId}
+          quantity={quantity}
+          productId={productId}
+          sizeId={sizeId}
+        />
+        <Typography className="text-lg">{currentQty}</Typography>
+        <UpdateIncrementQty
+          id={id}
+          bagId={bagId}
+          quantity={quantity}
+          productId={productId}
+          sizeId={sizeId}
+        />
       </div>
     </>
   );
+}
+
+function RemoveBagItems({ id }) {
+  const [deleteFromBagItems, { isLoading, isError, isSuccess, error }] =
+    useDeleteFromBagItemsMutation();
+
+  useEffect(() => {
+    if (isSuccess) {
+      console.log("Data Telah di Hapus");
+    }
+  }, [isSuccess]);
+
+  const handleRemoveFromCart = (id) => {
+    deleteFromBagItems({ id });
+    console.log("handleRemoveFromCart", id);
+  };
+
+  return (
+    <>
+      {isLoading ? (
+        <div className="w-[20px] flex justify-center items-start">
+          <Spinner className="h-[13px] w-[13px]" />
+        </div>
+      ) : (
+        <div
+          className="w-[20px] flex justify-center items-start"
+          onClick={() => handleRemoveFromCart(id)}
+        >
+          <XMarkIcon className="h-4 w-4 cursor-pointer" />
+        </div>
+      )}
+    </>
+  );
+}
+
+function ListCardProduct() {
+  const {
+    data: cartProduct,
+    isLoading,
+    isFetching,
+    isSuccess,
+    isError,
+    error,
+  } = useGetBagItemsQuery();
+
+  if (isLoading) {
+    return (
+      <>
+        <Spinner />
+      </>
+    );
+  }
+
+  if (isError) {
+    return (
+      <>
+        <EmptyState />
+      </>
+    );
+  }
+
+  if (isSuccess) {
+    return (
+      <>
+        <div className="">
+          {cartProduct.data.map((product, idx) => {
+            return (
+              <div key={idx} className="flex flex-col-3 mt-4 gap-2">
+                <div className="w-[120px] flex justify-between">
+                  <div className="flex justify-start items-start select-none">
+                    <Checkbox className="h-5 w-5" />
+                  </div>
+                  <div className="flex justify-start items-start select-none">
+                    <img
+                      src={product.product_image}
+                      alt="drawer1"
+                      className="h-[100px] w-[65px] object-cover rounded-md"
+                    />
+                  </div>
+                </div>
+                <div className="gap-1 w-[210px] select-none">
+                  <Typography className="text-sm">
+                    {product.product_name}
+                  </Typography>
+                  <Typography className="text-sm">
+                    <NumberFormatCurrency
+                      value={product.quantity * product.product_price}
+                    />
+                  </Typography>
+                  <Typography className="mt-3 text-sm">
+                    {product.size_name}
+                  </Typography>
+                  <QuantityItem
+                    id={product.id}
+                    bagId={product.bag_id}
+                    quantity={product.quantity}
+                    productId={product.product_id}
+                    sizeId={product.size_id}
+                  />
+                </div>
+                <RemoveBagItems id={product.id} />
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
 }
 export default ListCardProduct;
--- src/slice/cartSlice.js
+++ src/slice/cartSlice.js
@@ -4,45 +4,56 @@ const initialState = {
   products: [],
   priceTotal: 0,
   qtyTotal: 0,
+  bagId: 0,
 };
 
 export const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
+    setProducts: (state, action) => {
+      state.products = action.payload;
+    },
     addToCart: (state, action) => {
-      const product = state.products.find(
-        (product) =>
-          product.id === action.payload.id &&
-          product.size_id === action.payload.size_id
-      );
-      if (product) {
-        product.qty++;
-      } else {
-        state.products.push(action.payload);
-      }
+      // const product = state.products.find(
+      //   (product) =>
+      //     product.id === action.payload.id &&
+      //     product.size_id === action.payload.size_id
+      // );
+      // if (product) {
+      //   product.qty++;
+      // } else {
+      //   state.products.push(action.payload);
+      // }
     },
 
     removeFromCart: (state, action) => {
-      const findIndex = state.products.findIndex(
-        (product) =>
-          product.id === action.payload.id &&
-          product.size_id === action.payload.size_id
-      );
-      if (findIndex !== -1) {
-        state.products.splice(findIndex, 1);
-      }
+      // const findIndex = state.products.findIndex(
+      //   (product) =>
+      //     product.id === action.payload.id &&
+      //     product.size_id === action.payload.size_id
+      // );
+      // if (findIndex !== -1) {
+      //   state.products.splice(findIndex, 1);
+      // }
     },
 
-    setPriceTotal: (state) => {
-      state.priceTotal = state.products.reduce((prevValue, curValue) => {
-        return prevValue + curValue.qty * curValue.price;
-      }, 0);
+    setPriceTotal: (state, action) => {
+      // state.priceTotal = state.products.reduce((prevValue, curValue) => {
+      //   return prevValue + curValue.qty * curValue.price;
+      // }, 0);
+      state.priceTotal = action.payload;
     },
-    setQtyTotal: (state) => {
-      state.qtyTotal = state.products.reduce((prevValue, curValue) => {
-        return prevValue + curValue.qty;
-      }, 0);
+    setQtyTotal: (state, action) => {
+      // state.qtyTotal = state.products.reduce((prevValue, curValue) => {
+      //   return prevValue + curValue.qty;
+      // }, 0);
+      console.log("setQtyTotal", action.payload);
+      state.qtyTotal = action.payload;
+    },
+
+    setBagId: (state, action) => {
+      state.bagId = action.payload;
     },
 
     incrementQty: (state, action) => {
@@ -73,6 +84,8 @@ export const {
   setQtyTotal,
   incrementQty,
   decrementQty,
+  setBagId,
+  setProducts,
 } = cartSlice.actions;
 
 export default cartSlice.reducer;

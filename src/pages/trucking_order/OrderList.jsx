deffb29b7e26e2b6363a761803453a0d8869fb15
sitiintannuraeni <sitiintannuraeni@smkwikrama.sch.id>
2024-04-25 09:44:53 +0700

    feat : update trucking order

---
+++ src/pages/trucking_order/OrderList.jsx
@@ -0,0 +1,86 @@
+import { ArrowLeftIcon } from "@heroicons/react/16/solid";
+import { Typography } from "@material-tailwind/react";
+import Logo from "../../assets/logoDrawer.png";
+
+function OrderList() {
+  return (
+    <>
+      <div className="pb-10 lg:px-40 py-10">
+        <div className="flex justify-center items-center pb-3">
+          <img
+            src={Logo}
+            alt="logoDrawer"
+            width={50}
+            className="object-cover"
+          />
+        </div>
+        <div className="flex justify-center">
+          <Typography className="text-center  ">
+            WorldWhite Enterprice account
+          </Typography>
+        </div>
+        <div className="border-b-[1px] border-black/40 mt-5 w-full " />
+      </div>
+      <div className="flex justify-center py-10 -mt-16">
+        <div className="flex flex-cols-1 w-[550px]">
+          <div className="w-full">
+            <div className="flex justify-between">
+              <ArrowLeftIcon className="w-6 h-6 cursor-pointer" />
+              <div className="pr-[217px]">
+                <Typography className="text-lg font-semibold">
+                  Order List
+                </Typography>
+              </div>
+            </div>
+            <div className="border border-black/15 mt-4 px-4 rounded-md">
+              <div className="mt-3">
+                <div className="flex flex-col-3 space-x-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-7 h-7 text-[#989898]"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 0 0 4.25 22.5h15.5a1.875 1.875 0 0 0 1.865-2.071l-1.263-12a1.875 1.875 0 0 0-1.865-1.679H16.5V6a4.5 4.5 0 1 0-9 0ZM12 3a3 3 0 0 0-3 3v.75h6V6a3 3 0 0 0-3-3Zm-3 8.25a3 3 0 1 0 6 0v-.75a.75.75 0 0 1 1.5 0v.75a4.5 4.5 0 1 1-9 0v-.75a.75.75 0 0 1 1.5 0v.75Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <Typography className="mt-[1px] text-black/85">
+                    15 Maret 2024
+                  </Typography>
+                  <div class="px-2 mt-[2px] text-xs rounded-md h-[25px] bg-[#EAEAEA]">
+                    <Typography className="text-[10px] mt-[4px] text-black/45">
+                      Menunggu Pembayaran
+                    </Typography>
+                  </div>
+                </div>
+                <Typography className="lg:mt-2 mt-5 lg:text-sm text-xs text-black/90">
+                  MAL24031513556483272075308950
+                </Typography>
+                <div className="flex justify-end">
+                  <div className="lg:-mt-6 -mt-[16.8px]">
+                    <Typography className="lg:text-base text-xs font-medium text-black/70 flex justify-end">
+                      Total Amount
+                    </Typography>
+                    <Typography className="flex justify-end lg:text-base text-xs">
+                      Rp 130.500
+                    </Typography>
+                    <div onClick={() => navigate("/member-order")}>
+                      <Typography className="flex justify-end mt-2 py-3 underline cursor-pointer lg:text-base text-sm">
+                        View Details
+                      </Typography>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default OrderList;
--- src/pages/trucking_order/TruckingPage.jsx
+++ src/pages/trucking_order/TruckingPage.jsx
@@ -4,11 +4,19 @@ import {
   Input,
   Typography,
 } from "@material-tailwind/react";
-import Logo from "../../assets/logo1.png";
-import { PlusIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import Logo from "../../assets/logo-auth.png";
+import {
+  ArrowLeftIcon,
+  PlusIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
 import { useState } from "react";
 import { current } from "@reduxjs/toolkit";
+import OrderList from "./OrderList";
+import { useNavigate } from "react-router-dom";
 function TruckingPage() {
+  const navigate = useNavigate();
+  const [isSubmit, setIsSubmit] = useState(false);
   const [truckingOrders, setTruckingOrders] = useState([{ id: 1 }]);
 
   const handleAddInputTruckingOrders = () => {
@@ -21,63 +29,82 @@ function TruckingPage() {
   const handleRemoveInputTruckingOrders = (id) => {
     setTruckingOrders((current) => current.filter((item) => item.id !== id));
   };
+
   return (
     <>
-      <div className="lg:px-[245px] px-3 py-14">
-        <div className="pb-10">
-          <div className="flex justify-center items-center pb-3">
-            <img
-              src={Logo}
-              alt="logo"
-              className="h-[122px] w-[150px] object-cover"
-            />
-          </div>
-          <div className="grid grid-rows justify-center mt-2">
-            <Typography className="text-center">MEN’S FASHION STORE</Typography>
-          </div>
-          <div className="border-b-[1px] border-black/20 mt-6 w-full" />
-          <Typography className="flex justify-center items-center mt-6">
-            Order Number Tracking
-          </Typography>
+      {isSubmit ? (
+        <OrderList />
+      ) : (
+        <div className="lg:px-[245px] px-3 py-14">
+          <div className="pb-8">
+            <div className="flex justify-center items-center pb-3 -mt-10">
+              <img
+                src={Logo}
+                alt="logo"
+                className="h-[200px] w-[200px] object-contain"
+              />
+            </div>
+            <div className="grid grid-rows justify-center -mt-6">
+              <Typography className="text-center text-white font-medium text-lg">
+                MELANCONNY
+              </Typography>
+            </div>
+            <div className="border-b-[1px] border-white mt-6 w-full" />
+            <Typography className="flex justify-center items-center mt-6 text-white">
+              Order Number Tracking
+            </Typography>
 
-          <div className="flex flex-col gap-2">
-            {truckingOrders.map((truckingOrder, index) => {
-              const isLastItem =
-                index + 1 === truckingOrders.length ||
-                truckingOrders.length === 1;
-              return (
-                <div
-                  className="flex justify-center flex-col-2 items-center"
-                  key={index}
-                >
-                  <div className="lg:w-80 w-64">
-                    <Input variant="static" placeholder="Insert order number" />
-                  </div>
-                  <IconButton
-                    variant="outlined"
-                    className="rounded-md lg:h-8 lg:w-8 h-7 w-7 mt-2"
-                    onClick={() =>
-                      isLastItem
-                        ? handleAddInputTruckingOrders()
-                        : handleRemoveInputTruckingOrders(truckingOrder.id)
-                    }
+            <div className="flex flex-col gap-2">
+              {truckingOrders.map((truckingOrder, index) => {
+                const isLastItem =
+                  index + 1 === truckingOrders.length ||
+                  truckingOrders.length === 1;
+                return (
+                  <div
+                    className="flex justify-center flex-col-2 items-center"
+                    key={index}
                   >
-                    {isLastItem ? (
-                      <PlusIcon className="h-4 w-4" />
-                    ) : (
-                      <XMarkIcon className="h-4 w-4" />
-                    )}
-                  </IconButton>
-                </div>
-              );
-            })}
-          </div>
+                    <div className="lg:w-80 w-64">
+                      <Input
+                        variant="static"
+                        placeholder="Insert order number"
+                        color="white"
+                      />
+                    </div>
+                    <IconButton
+                      variant="outlined"
+                      color="white"
+                      className="rounded-md lg:h-8 lg:w-8 h-7 w-7 mt-2"
+                      onClick={() =>
+                        isLastItem
+                          ? handleAddInputTruckingOrders()
+                          : handleRemoveInputTruckingOrders(truckingOrder.id)
+                      }
+                    >
+                      {isLastItem ? (
+                        <PlusIcon className="h-4 w-4 text-white" />
+                      ) : (
+                        <XMarkIcon className="h-4 w-4 text-white" />
+                      )}
+                    </IconButton>
+                  </div>
+                );
+              })}
+            </div>
 
-          <div className="flex justify-center items-center mt-6">
-            <Button className="bg-[#D9D9D9] lg:w-64 w-56">SUBMIT</Button>
+            <div className="flex justify-center items-center mt-6">
+              <Button
+                className="bg-[#FFFFFF] lg:w-64 w-56"
+                onClick={() => setIsSubmit(true)}
+              >
+                <Typography className="text-black text-base font-bold">
+                  SUBMIT
+                </Typography>
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
